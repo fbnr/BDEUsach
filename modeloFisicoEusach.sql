63b42f80da7a76e599157f2ae96bade6e500e592
fbnr <fbn.ramirezreveco@gmail.com>
2016-05-16 16:26:05 -0300

    agregando script de BD y archivos cdm

---
+++ modeloFisicoEusach.sql
@@ -0,0 +1,171 @@
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     16/05/2016 2:25:08                           */
+/*==============================================================*/
+
+
+drop table if exists COMENTARIO;
+
+drop table if exists EVENTO;
+
+drop table if exists EVENTOUSUARIO;
+
+drop table if exists LUGAR;
+
+drop table if exists PREFERENCIAS;
+
+drop table if exists TIPO;
+
+drop table if exists TIPOESTADO;
+
+drop table if exists USUARIO;
+
+drop table if exists USUARIOSEGUIDOS;
+
+/*==============================================================*/
+/* Table: COMENTARIO                                            */
+/*==============================================================*/
+create table COMENTARIO
+(
+   ID_COMENTARIO        int not null auto_increment,
+   ID_USUARIO           int not null,
+   ID_EVENTO            int not null,
+   TEXTO_COMENTARIO     text,
+   FECHA_COMENTARIO     datetime,
+   primary key (ID_COMENTARIO)
+);
+
+/*==============================================================*/
+/* Table: EVENTO                                                */
+/*==============================================================*/
+create table EVENTO
+(
+   ID_EVENTO            int not null auto_increment,
+   ID_LUGAR             int not null,
+   ID_TIPO              int not null,
+   ID_USUARIO           int not null,
+   TITULO_EVENTO        varchar(50),
+   INICIO_EVENTO        time,
+   FIN_EVENTO           time,
+   FECHA_EVENTO         date,
+   PUBLICO_OBJETIVO     varchar(50),
+   DESCRIPCION_EVENTO   text,
+   primary key (ID_EVENTO)
+);
+
+/*==============================================================*/
+/* Table: EVENTOUSUARIO                                         */
+/*==============================================================*/
+create table EVENTOUSUARIO
+(
+   ID_EVENTO_USUARIO    int not null auto_increment,
+   ID_EVENTO            int,
+   ID_USUARIO           int not null,
+   primary key (ID_EVENTO_USUARIO)
+);
+
+/*==============================================================*/
+/* Table: LUGAR                                                 */
+/*==============================================================*/
+create table LUGAR
+(
+   ID_LUGAR             int not null auto_increment,
+   NOMBRE_LUGAR         varchar(50),
+   COORDENADA_X         float,
+   COORDENADA_Y         float,
+   primary key (ID_LUGAR)
+);
+
+/*==============================================================*/
+/* Table: PREFERENCIAS                                          */
+/*==============================================================*/
+create table PREFERENCIAS
+(
+   ID_PREFERENCIA       int not null auto_increment,
+   ID_TIPO              int not null,
+   ID_USUARIO           int not null,
+   primary key (ID_PREFERENCIA)
+);
+
+/*==============================================================*/
+/* Table: TIPO                                                  */
+/*==============================================================*/
+create table TIPO
+(
+   ID_TIPO              int not null auto_increment,
+   TIPO_EVENTO          varchar(50),
+   DESCRIPCION          text,
+   primary key (ID_TIPO)
+);
+
+/*==============================================================*/
+/* Table: TIPOESTADO                                            */
+/*==============================================================*/
+create table TIPOESTADO
+(
+   ID_TIPOESTADO        int not null auto_increment,
+   NOMBRE_ESTADO        varchar(50),
+   DESCRIPCION_ESTADO   text,
+   primary key (ID_TIPOESTADO)
+);
+
+/*==============================================================*/
+/* Table: USUARIO                                               */
+/*==============================================================*/
+create table USUARIO
+(
+   ID_USUARIO           int not null auto_increment,
+   ID_TIPOESTADO        int not null,
+   NOMBRE_USUARIO       varchar(50),
+   CORREO_USUARIO       varchar(50),
+   CARRERA_USUARIO      varchar(50),
+   CONTRASENHA_USUARIO  varchar(50),
+   ESADMINISTRADOR_     bool,
+   primary key (ID_USUARIO)
+);
+
+/*==============================================================*/
+/* Table: USUARIOSEGUIDOS                                       */
+/*==============================================================*/
+create table USUARIOSEGUIDOS
+(
+   ID_USUARIO           int not null,
+   USU_ID_USUARIO       int not null
+);
+
+alter table COMENTARIO add constraint FK_EXISTE_PARA foreign key (ID_EVENTO)
+      references EVENTO (ID_EVENTO) on delete restrict on update restrict;
+
+alter table COMENTARIO add constraint FK_PUEDE_CREAR foreign key (ID_USUARIO)
+      references USUARIO (ID_USUARIO) on delete restrict on update restrict;
+
+alter table EVENTO add constraint FK_CREA foreign key (ID_USUARIO)
+      references USUARIO (ID_USUARIO) on delete restrict on update restrict;
+
+alter table EVENTO add constraint FK_POSEE foreign key (ID_LUGAR)
+      references LUGAR (ID_LUGAR) on delete restrict on update restrict;
+
+alter table EVENTO add constraint FK_SE_COMPONE foreign key (ID_TIPO)
+      references TIPO (ID_TIPO) on delete restrict on update restrict;
+
+alter table EVENTOUSUARIO add constraint FK_ASISTE foreign key (ID_USUARIO)
+      references USUARIO (ID_USUARIO) on delete restrict on update restrict;
+
+alter table EVENTOUSUARIO add constraint FK_TIENE foreign key (ID_EVENTO)
+      references EVENTO (ID_EVENTO) on delete restrict on update restrict;
+
+alter table PREFERENCIAS add constraint FK_ESTA_EN foreign key (ID_TIPO)
+      references TIPO (ID_TIPO) on delete restrict on update restrict;
+
+alter table PREFERENCIAS add constraint FK_MANTIENE foreign key (ID_USUARIO)
+      references USUARIO (ID_USUARIO) on delete restrict on update restrict;
+
+alter table USUARIO add constraint FK_TIENE_UN foreign key (ID_TIPOESTADO)
+      references TIPOESTADO (ID_TIPOESTADO) on delete restrict on update restrict;
+
+alter table USUARIOSEGUIDOS add constraint FK_PUEDE_SEGUIR_A foreign key (ID_USUARIO)
+      references USUARIO (ID_USUARIO) on delete restrict on update restrict;
+
+alter table USUARIOSEGUIDOS add constraint FK_SEGUIDO_POR foreign key (USU_ID_USUARIO)
+      references USUARIO (ID_USUARIO) on delete restrict on update restrict;
+
---
+++ poblacionEUsach.sql
@@ -0,0 +1,79 @@
+/* Hay 3 tipos de estado no más, no sé cuantos más pueden haber*/
+insert into TipoEstado (nombre_estado, descripcion_estado)
+values
+('Habilitado', 'Este estado representa a los usuarios activos'),
+('Deshabilitado', 'Este estado representa a los usuarios inactivos por voluntado propia'),
+('Bloqueado', 'Este estado representa a los usuarios que fueron bloqueados por algún administrador');
+
+/* No se me ocurren más cosas, pero aquí va la idea*/
+insert into Tipo (tipo_evento, descripcion)
+values
+('Concierto', 'Un concierto donde muchos cantan y tocan distintos instrumentos.'),
+('Obra de teatro', 'Representación artística con personificación de personajes.'),
+('Ceremonia de titulación', 'Ceremonia donde a alumnos con todos sus ramos y tesis aprobada, reciben su certificado para ejercer su profesión.'),
+('Exposición', 'Exposición donde una persona expone un tema de investigación de mucha experiencia para él.');
+
+/* Distintos lugares de la universidad donde se hacen cosas culturales y artisticas */
+insert into Lugar (nombre_lugar, coordenada_x, coordenada_y)
+values
+('Aula Magna', , ), /*falta agregar las coordenadas de los lugares */
+('Cite-Camp', , ),
+('Departamento de Ingeniería en Informática', , ),
+('Foro Griego', , );
+
+/* Existen 5 usuarios random y nosotros, como administradores de la aplicacion*/
+insert into Usuario (id_tipoestado, nombre_usuario, correo_usuario, carrera_usuario, contrasenha_usuario, esAdministrador?)
+values
+(1, 'Jonathan Simpson', 'jsimpson0@ebay.com', 'Administrative Officer', 'usach', false),
+(1, 'Dennis Cooper', 'dcooper1@abc.net.au', 'Software Consultant', 'usach', false),
+(1, 'Steve Welch', 'swelch2@wufoo.com', 'Software Consultant', 'usach', false),
+(1, 'Judy Price', 'jprice3@canalblog.com', 'Editor', 'usach', false),
+(1, 'Robert Foster', 'rfoster4@is.gd', 'Marketing Manager', 'usach', false),
+(1, 'Pablo Ulloa', 'pablo.ulloa@usach.cl', 'Ingeniería Ejecución en Informática', 'hola123', true),
+(1, 'Francisco Lopez', 'francisco.lopez.a@usach.cl', 'Ingeniería Ejecución en Informática', 'hola123', true),
+(1, 'Dania Montanares', 'dania.montanares@usach.cl', 'Ingeniería Civil en Informática', 'hola123', true),
+(1, 'Fabian Ramirez', 'fabian.ramirezr@usach.cl', 'Ingeniería Ejecución en Informática', 'hola123', true),
+(1, 'Bastian Toro', 'bastian.toro@usach.cl', 'Ingeniería Civil en Informática', 'hola123', true);
+
+/* Usuarios que se siguen entre sí */
+insert into UsuarioSeguidos ()
+values
+(), /* no se como se colocan */
+();
+
+/* Preferencias que tiene un usuario en base al tipo de eventos que existen */
+insert into Preferencias (id_tipo, id_usuario)
+values
+(1, 1), /* al usuario 1 le gustan el tipo 1: concierto */
+(2, 1), /* el usuario 1 tambien tiene preferencias con las obras de teatro */
+(1, 2),
+(4, 2),
+(4, 3),
+(3, 3),
+(4, 4),
+(1, 5),
+(2, 5);
+
+/* Evento culturales o artisticos que existen en la u */
+insert into Evento (id_lugar, id_tipo, id_usuario, titulo_evento, inicio_evento, fin_evento, fecha_evento, publico_objetivo, descripcion_evento)
+values
+(1, 1, 9, 'Orquesta sinfónica Usach', '19:00:00', '21:00:00', '2016/05/23', 'Esto va orientado para los amantes de la musica (?)', 'Concierto de la orquesta sinfonica de la usach para darse a conocer al estudiantado y presentar sus nuevas melodias (?).'),
+(3, 3, 6, 'Nuevos profesionales de la Informática', '19:00:00', '21:00:00', '2016/05/26', 'Esto va orientado para los que quieren ver como se titulan (?)', 'Ceremonia de titulacion donde se presentaran a los nuevos profesionales del futuro.(?)');
+
+/* tabla intermedia de los eventos, ya que un usuario puede asistir a más de un evento y a un evento pueden asistir muchos usuarios */
+insert into EventoUsuario (id_evento, id_usuario)
+values
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 4),
+(2, 5),
+(1, 5),
+(2, 1);
+
+/* comentarios de usuarios hacia eventos existentes */
+insert into Comentario (id_usuario, id_evento, texto_comentario, fecha_comentario)
+values
+(1, 1, 'Me encanta la música clásica!.', '2016/05/15 23:15:00'),
+(4, 2, 'Pues podré ver como se realiza una ceremonia de titulación.', '2016/05/14 19:15:00'),
+(3, 1, 'No me gusta mucho, pero veremos que tal', '2016/05/13 08:30:00');
